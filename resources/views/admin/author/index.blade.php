62c0970a8b9fbb0d346239163f4d0f12882bee76
lianmafutra <lianmafutra@gmail.com>
2020-03-05 07:46:18 -0800

    Menambah view data table author

--- resources/views/admin/author/index.blade.php
+++ resources/views/admin/author/index.blade.php
@@ -2,4 +2,35 @@
 
 @section('content')
     <h1>Penulis</h1>
+    <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">DataTable with default features</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          <table id="example1" class="table table-bordered table-striped">
+            <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nama</th>
+              <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>1</td>
+              <td>lian
+              </td>
+              <td>edit|hapus
+            </td>
+              
+            </tr>
+           
+            </tbody>
+           
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+    
 @endsection
--- routes/admin.php
+++ routes/admin.php
@@ -1,2 +1,7 @@
 <?php
+
+use App\Http\Controllers\Admin\AuthorController;
+
 Route::get('/','HomeController@index')->name('admin');
+
+Route::get('/author','AuthorController@index')->name('author.index');
