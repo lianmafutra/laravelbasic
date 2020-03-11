14740461cee1f05b140b8702e90383436fc4cfdb
lianmafutra <lianmafutra@gmail.com>
2020-03-11 15:08:27 +0700

    @push datatable

--- resources/views/admin/templates/partials/head.blade.php
+++ resources/views/admin/templates/partials/head.blade.php
@@ -13,18 +13,13 @@
     <link rel="stylesheet" href="{{asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- iCheck -->
     <link rel="stylesheet" href="{{asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/jqvmap/jqvmap.min.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('assets/dist/css/adminlte.min.css')}}">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/daterangepicker/daterangepicker.css')}}">
-    <!-- summernote -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/summernote/summernote-bs4.css')}}">
-    {{-- data Table --}}
-    <link rel="stylesheet" href="{{asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.css')}}">
-    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">  
+     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  
+  @stack('css_datatable')
+  
   </head>
--- resources/views/admin/templates/partials/script.blade.php
+++ resources/views/admin/templates/partials/script.blade.php
@@ -32,9 +32,5 @@
 <script src="{{asset('assets/dist/js/pages/dashboard.js')}}"></script>>
 <!-- AdminLTE for demo purposes -->
 <script src="{{asset('assets/dist/js/demo.js')}}"></script>
-<!-- DataTables -->
-<script src="{{asset('assets/plugins/datatables/jquery.dataTables.js')}}"></script>
-<script src="{{asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
-
 
 @stack('scripts')
