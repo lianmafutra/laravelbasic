19c7ab64e1e146c929993c176457cf5e54292a2f
lianmafutra <lianmafutra@gmail.com>
2020-03-14 21:53:44 -0700

    mengubah tampilan register ke materialize

--- resources/views/frontend/template/partials/navigation.blade.php
+++ resources/views/frontend/template/partials/navigation.blade.php
@@ -3,8 +3,8 @@
       <a href="#!" class="brand-logo">Perpustakaan</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">Login</a></li>
-        <li><a href="badges.html">Register</a></li>
+      <li><a href={{route('login')}}>Login</a></li>
+        <li><a href={{ route('register') }}>Register</a></li>
       </ul>
     </div>
   </nav>
